6e93b04a4f7748260b4dfd56445257e83153ccf7
Nikoo Farsi <nikoo@Nikoos-MacBook-Pro.local>
2024-06-19 12:39:36 +0100

    Added login page

---
+++ components/login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <section className="login">
+      <h2>Login as Paul?</h2>
+      <button className="login-button">Login</button>
+      <p>I'm not paul</p>
+    </section>
+  );
+};
+
+export default Login;
