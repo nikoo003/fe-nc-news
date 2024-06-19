84ae327a944c44846098eb9986ded7ad1542bcd5
Nikoo Farsi <nikoo@Nikoos-MacBook-Pro.local>
2024-06-19 20:18:20 +0100

    wrap App component with BrowserRouter to resolve useRoutes error

--- components/login.jsx
+++ components/login.jsx
@@ -1,22 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import { getLogin } from "../src/utils/api";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-
-const navigate = useNavigate()
-
-function handleSubmit(){
-    getLogin().then(()=>{
-        navigate("/topics")
-    }).catch((err)=>{
-        console.log("login failed", err)
-    })
-}
 
   return (
     <section className="login">
       <h2>Login as Paul?</h2>
-      <button className="login-button" onClick={handleSubmit}>Login</button>
+      <Link to={"/topics"}>
+      <button className="login-button">Login</button>
+      </Link>
       <p>I'm not paul</p>
     </section>
   );
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const ncNews = axios.create({
-  baseURL: "https://nc-news-g7c7.onrender.com"
+  baseURL: "https://nc-news-g7c7.onrender.com/api",
 });
 
 export function getLogin() {
-  return ncNews.get("/api/").then((res) => {
-    res.data;
-  });
+  return ncNews
+    .get("/")
+    .then((res) => {
+      console.log(res);
+      res.data;
+    })
+    .catch((err) => {
+      console.log("errooorrrr", err);
+    });
 }
