23aa088438a65787464d997e096d5dc8860bb8aa
Nikoo Farsi <nikoo@nikoos-mbp.home>
2024-06-21 00:30:34 +0100

    Added total voted of all commenters

--- components/comments.jsx
+++ components/comments.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import { getCommentsByArticleId } from "../src/utils/api";
 import { useParams } from "react-router-dom";
-import "../styling/comments.css"
+import "../styling/comments.css";
 
 function CommentsByArticleId() {
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [totalVotes, setTotalVotes] = useState(0);
   const { article_id } = useParams();
 
   useEffect(() => {
     getCommentsByArticleId(article_id).then((data) => {
+      let totalVotes = 0;
+      data.forEach((comment) => {
+        totalVotes += comment.votes;
+      });
+      setTotalVotes(totalVotes);
       setComments(data);
       setLoading(false);
     });
@@ -21,13 +26,14 @@ function CommentsByArticleId() {
   }
 
   return (
-    <section >
+    <section>
+      <h3>Comments</h3>
+      <p>Total votes: {totalVotes}</p>
       {comments.map((comment) => {
         return (
           <ul className="comments" key={comment.comment_id}>
             <p>{comment.body}</p>
             <p>-{comment.author}</p>
-            <p>votes: {comment.votes}</p>
           </ul>
         );
       })}
