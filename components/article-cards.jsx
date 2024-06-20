5f38b5ec556b5621c709c690dc5051a170d72d01
Nikoo Farsi <nikoo@Nikoos-MacBook-Pro.local>
2024-06-20 12:07:19 +0100

    Added isLoading functionality

--- components/article-cards.jsx
+++ components/article-cards.jsx
@@ -1,24 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getArticles } from "../src/utils/api";
-import "../styling/article-cards.css"
+import "../styling/article-cards.css";
 
 function ArticleCards({ articles, setArticles }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     getArticles().then((data) => {
       setArticles(data);
+      setIsLoading(false);
     });
   }, []);
 
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
-    <section >
+    <section>
       <h3>All articles</h3>
       <ul>
         {articles.map((article) => {
           return (
-            <li key={article.title} className="article-card" >
-              <img src={article.article_img_url}/>
+            <li key={article.title} className="article-card">
+              <img src={article.article_img_url} />
               <h4>{article.title}</h4>
-              <p><em>{article.topic}</em></p>
+              <p>
+                <em>{article.topic}</em>
+              </p>
             </li>
           );
         })}
